8aa2f15fc8ec5738f2a0b0e70d1d5c70c5da61c8
m37moud <m37moud00@Gmail.com>
2024-01-29 19:31:45 +0200

    init

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,7 +43,7 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.core:core-ktx:1.10.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
@@ -66,7 +66,7 @@ dependencies {
     implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.4.0")
     implementation ("androidx.lifecycle:lifecycle-runtime-ktx:2.4.0")
 
-    implementation ("androidx.activity:activity-ktx:1.3.1")
+    implementation ("androidx.activity:activity-ktx:1.7.2")
 
     implementation ("androidx.recyclerview:recyclerview:1.2.1")
 
